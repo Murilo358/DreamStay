d8dd22513bae40e5b6e90eacccb6be22880761f6
Murilo358 <murilobarbosa358@gmail.com>
2023-07-05 18:31:42 -0300

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+"use client";
+
+import { data } from "autoprefixer";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  //useSession pega o user da context
+  const { data } = useSession();
+
+  return <div></div>;
 }
