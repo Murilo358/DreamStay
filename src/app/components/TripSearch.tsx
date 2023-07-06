f5cedc53036a67910a0d2c17e6941953969d891c
Murilo358 <murilobarbosa358@gmail.com>
2023-07-05 23:19:07 -0300

    feat: add quick search component

---
+++ src/app/components/TripSearch.tsx
@@ -0,0 +1,31 @@
+"use client ";
+import React from "react";
+import Input from "@/components/Input";
+import DatePicker from "@/components/DatePicker";
+import CurrencyInput from "@/components/CurrencyInput";
+import Button from "@/components/Button";
+
+const TripSearch = () => {
+  return (
+    <div className="container mx-auto bg-search-background bg-cover bg-center bg-no-repeat">
+      <h1 className=" text-center font-semibold text-2xl text-grayPrimary">
+        Encontre sua proxima <span className="text-primary">viagem!</span>
+      </h1>
+
+      <div className="flex flex-col gap-4 mt-5">
+        <Input placeholder="Onde você quer ir?" />
+        <div className="flex gap-4">
+          <DatePicker
+            className="w-full"
+            onChange={() => {}}
+            placeholderText="Data de ida"
+          />
+          <CurrencyInput placeholder="Orçamento" />
+        </div>
+        <Button>Buscar</Button>
+      </div>
+    </div>
+  );
+};
+
+export default TripSearch;
