4e606acba2ecb32e80472a6485cfcf95407b3391
Murilo358 <murilobarbosa358@gmail.com>
2023-07-07 18:58:53 -0300

    feat: added input requirements

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -5,12 +5,27 @@ import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 import { Trip } from "@prisma/client";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 interface TripReservationProps {
   trip: Trip;
 }
 
+interface TripReservationForm {
+  guests: number;
+}
+
 const TripReservation = ({ trip }: TripReservationProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TripReservationForm>();
+
+  const onSubmit = (data: any) => {
+    console.log({ data });
+  };
+
   return (
     <div>
       <div className="flex flex-col px-5">
@@ -27,8 +42,15 @@ const TripReservation = ({ trip }: TripReservationProps) => {
           />
         </div>
         <Input
+          //prettier-ignore
+          {...register("guests", { required: {
+            value: true,
+            message: "Por favor informe o número de hóspedes"
+          }})}
           className="mt-4"
           placeholder={`Número máximo de hóspedes (${trip.maxGuests})`}
+          error={!!errors?.guests}
+          errorMessage={errors?.guests?.message}
         />
         <div className="flex justify-between mt-3">
           <p className="font-medium text-sm text-primaryDarker">Total: </p>
@@ -38,7 +60,12 @@ const TripReservation = ({ trip }: TripReservationProps) => {
           className=" pb-10 border-b border-grayLighter
         "
         >
-          <Button className="w-full mt-3">Reservar agora!</Button>
+          <Button
+            onClick={() => handleSubmit(onSubmit)()}
+            className="w-full mt-3"
+          >
+            Reservar agora!
+          </Button>
         </div>
       </div>
     </div>
