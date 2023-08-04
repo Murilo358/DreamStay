0bb9708aa771b83cff947b912b45b7a72ace6049
Murilo358 <murilobarbosa358@gmail.com>
2023-08-03 22:50:50 -0300

    updated: sucess_Url

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
   } = req;
 
   const session = await stripe.checkout.sessions.create({
-    success_url: "https://dreamstay.vercel.app",
+    success_url: "https://dream-stay.vercel.app",
     metadata: {
       startDate,
       endDate,
