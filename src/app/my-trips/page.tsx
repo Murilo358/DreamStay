378b7f2ec35a713181698142bf0442064e6c04dd
Murilo358 <murilobarbosa358@gmail.com>
2023-08-03 22:15:13 -0300

    fixed: my-trips-page

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,6 +1,4 @@
-//localhost:3000
-
-http: "use client";
+"use client";
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
